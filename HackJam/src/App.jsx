205246bc4423204f6c5849d32d7a3d6e44bbe3aa
Asadbek <asadbekn443@gmail.com>
2023-09-26 21:45:15 -0400

    Components

--- HackJam/src/App.jsx
+++ HackJam/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './components/Home'
+import FAQ from './components/FAQ'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,6 +27,8 @@ function App() {
         <p>
           Hello world
         </p>
+        <Home/>
+        <FAQ/>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
---
+++ HackJam/src/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Home(){
+return(
+    <h1 className="bg-red-700">Hello World!</h1>
+);
+}
+
+export default Home;
