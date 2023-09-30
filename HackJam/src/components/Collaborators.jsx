8c57121e54dcad5a8103f5e220d1fa960b8c3a9f
Quinn <quinnberichon@gmail.com>
2023-09-30 08:25:48 -0400

    Adjusted heights of page section

--- HackJam/src/components/Collaborators.jsx
+++ HackJam/src/components/Collaborators.jsx
@@ -7,33 +7,26 @@ import GoogleLogo from "../assets/GoogleLogo.png"
 function Collaborators() {
   return (
     <>
-      <div id="collaborators" className="h-screen">
+      <div id="collaborators" className="h-min mb-20">
         <h1 className="m-auto">Brought to you in collaboration with...</h1>
         <br/><br/>
         <div className="flex flex-row select-none pr-10">
           <div className="flex-auto p-0 w-20 m-auto ml-8 ">
-            <img src={MetaLogo} className="stu-org-logo" alt="Logo" />
+            <img src={MetaLogo} alt="Logo" />
           </div>
           <div className="flex-auto p-0 w-20 m-auto ml-8 max-h-30">
-            <img src={AppleLogo} className="stu-org-logo" alt="Logo" />
+            <img src={AppleLogo} alt="Logo" />
           </div>
           <div className="flex-auto p-0 w-20 m-auto ml-8 ">
-            <img src={AmazonLogo} className="stu-org-logo" alt="Logo" />
+            <img src={AmazonLogo} alt="Logo" />
           </div>
           <div className="flex-auto p-0 w-20 m-auto ml-8 ">
-            <img src={NetflixLogo} className="stu-org-logo" alt="Logo" />
+            <img src={NetflixLogo} alt="Logo" />
           </div>
           <div className="flex-auto p-0 w-20 m-auto ml-8 ">
-            <img src={GoogleLogo} className="stu-org-logo" alt="Logo" />
+            <img src={GoogleLogo} alt="Logo" />
           </div>
         </div>
-
-
-
-
-
-
-        
       </div>
     </>
   );
