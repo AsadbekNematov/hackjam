a13f672dcba5811ca76738fb6a10063e53ae9e38
Quinn <quinnberichon@gmail.com>
2023-10-02 14:26:47 -0400

    Fixed about text

--- HackJam/src/components/About.jsx
+++ HackJam/src/components/About.jsx
@@ -2,7 +2,7 @@ import photoHex from '../assets/hex_photo_gallery.png'
 
 function About() {
   return (
-    <div id="about" className="section">
+    <div id="about" className="section" style={{'margin-top': '2rem'}}>
       <h1 className="m-auto motto">Think. Code. Innovate.</h1>
       <br/><br/>
       <div className="about">
@@ -13,9 +13,9 @@ function About() {
           <p className="text-left text-lg">Welcome to HackJam - Where Innovation Meets Code!</p>
           <br/>
           <p className="text-left text-lg">
-            Our hackathon is a dynamic platform where brilliant minds come together to transform ideas into reality. 
+            Our hackathon is a dynamic environment where brilliant minds come together to transform ideas into reality. 
             Whether you're a seasoned coder or a passionate novice, HackJam offers an exhilarating opportunity to think outside the box, write innovative code, and create 
-            solutions that can change the world. Join us for ***[CHANGE THIS]*** hours of intense coding, collaboration, and innovation as we embrace the motto 'Think. Code. Innovate.' 
+            solutions that can change the world. Join us for 17 hours of intense coding, collaboration, and innovation as we embrace the motto 'Think. Code. Innovate.' 
             Together, we'll turn dreams into tangible projects and propel the future of technology. Get ready to jam with us and code your way to greatness!
           </p>
         </div>
--- HackJam/src/components/Home.jsx
+++ HackJam/src/components/Home.jsx
@@ -2,22 +2,25 @@ import Navbar from "./Navbar";
 
 function Home() {
   return (
-    <div className="h-screen select-none flex">
-      <Navbar/>
-      <div className=" m-auto flex-col">
-        <h1 className="m-auto font-bold hover:ease-in-out dark-color tt text-9xl">HACKJAM</h1>
-        <h3 className="m-auto font-bold dark-color tt text-6xl">2023</h3>
-        <br/>
-        <br/>
-        <h2 class="" style={{width: "100%"}}>October 22<sup>nd</sup></h2>
-        <br/>
-        <form action="https://en.wikipedia.org/wiki/Hot_dog">
-          <button onClick="" class="btn bg-transparen font-semibold py-2 px-4 border btn-border hover:border-transparent rounded">
-            Register Now
-          </button>
-        </form>
+    <>
+      <div className="h-screen select-none flex">
+        <Navbar/>
+        <div className=" m-auto flex-col">
+          <h1 className="m-auto font-bold hover:ease-in-out dark-color tt text-9xl">HACKJAM</h1>
+          <h3 className="m-auto font-bold dark-color tt text-6xl">2023</h3>
+          <br/>
+          <br/>
+          <h2 class="" style={{width: "100%"}}>October 22<sup>nd</sup></h2>
+          <br/>
+          <form action="https://en.wikipedia.org/wiki/Hot_dog">
+            <button onClick="" class="btn bg-transparen font-semibold py-2 px-4 border btn-border hover:border-transparent rounded">
+              Register Now
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+      <br/><br/>
+    </>
   );
 }
 
