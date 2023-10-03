43c502a86fcfaee2e9ced6b830faa06dd8a2607e
Quinn <quinnberichon@gmail.com>
2023-10-03 10:08:08 -0400

    Fixed faq, increased size of date

--- HackJam/src/components/QA.jsx
+++ HackJam/src/components/QA.jsx
@@ -10,7 +10,7 @@ function QA({question, answer}) {
   return (
     <div className={container}>
       <div className="p-4 text-xl relative font-medium flex">
-        <div className="text-left w-5/6 flex-auto">
+        <div className="text-left w-5/6 flex-auto q-text">
           {question}
         </div>
         <button
