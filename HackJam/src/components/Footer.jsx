ef7c41c38a55eaf625de930708ab3e684d0b54ab
Asadbek <asadbekn443@gmail.com>
2023-10-03 18:18:24 -0400

    SocialMedia

--- HackJam/src/components/Footer.jsx
+++ HackJam/src/components/Footer.jsx
@@ -1,26 +1,23 @@
 import HackJamLogo from '../assets/logo.png'
 function Footer() {
   return (
-    <div id="socials" className="h-2 pb-10 mb-20">
-      <img src={HackJamLogo} className="mx-auto h-24 select-none" alt='logo' />
-      
-      
-      
-      
+    <div id="socials" className="h-2 pb-30 mb-20">
       {/* <p>For questions and concerns, reach out to us on one of our platforms</p>
-      <br/><br/>
-      <div class="flex justify-center">
-        <div class="flex-none w-24 h-24">
+      <br/><br/> */}
+      <div className="flex justify-center">
+        <div className="flex-none w-24 h-24 m-0">
           <a href={"https://www.facebook.com/hackabull"}>
             <i className="fa-brands fa-instagram socials text-3xl"/>
           </a>
         </div>
-        <div class="flex-none w-24 h-24">
+        <div className="flex-none m-0 w-24 h-24">
           <a href={"https://www.facebook.com/hackabull"}>
             <i className="fa-brands fa-discord socials text-3xl"/>
           </a>
         </div>
-      </div> */}
+      </div> 
+      <a href="#home">
+      <img src={HackJamLogo} className="mx-auto h-24 select-none" alt='logo' /></a>
     </div>
   );
 }
