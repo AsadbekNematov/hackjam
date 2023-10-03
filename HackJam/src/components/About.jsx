68b59fe34b90a3f66675f3d5a2d6bc29dd0b1cae
Quinn <quinnberichon@gmail.com>
2023-10-02 21:45:09 -0400

    Updated about section

--- HackJam/src/components/About.jsx
+++ HackJam/src/components/About.jsx
@@ -16,7 +16,7 @@ function About() {
             <p className="text-left text-lg">
               Our hackathon is a dynamic environment where brilliant minds come together to transform ideas into reality. 
               Whether you're a seasoned coder or a passionate novice, HackJam offers an exhilarating opportunity to think outside the box, write innovative code, and create 
-              solutions that can change the world. Join us for 17 hours of intense coding, collaboration, and innovation as we embrace the motto 'Think. Code. Innovate.' 
+              solutions that can change the world. Join us for 12 hours of intense coding, collaboration, and innovation as we embrace the motto 'Think. Code. Innovate.' 
               Together, we'll turn dreams into tangible projects and propel the future of technology. Get ready to jam with us and code your way to greatness!
             </p>
           </div>
