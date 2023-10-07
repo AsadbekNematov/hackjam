16a73b4ba7d7cfec352c2ebd842be060e3b16de4
Marsel Fetlyaev <fetmarsel2002@gmail.com>
2023-10-07 18:27:30 -0400

    schedule final

--- HackJam/src/components/Schedule.jsx
+++ HackJam/src/components/Schedule.jsx
@@ -17,7 +17,7 @@ function Schedule() {
           <tbody>
             <tr>
               <td data-label="Time">8 AM - 11 AM</td>
-              <td data-label="Event">Check-In Begins</td>
+              <td data-label="Event">Check-In Time</td>
               <td data-label="Organizer">Everyone</td>
               <td data-label="Location">HALL OF FLAGS (ENB II)</td>
             </tr>
@@ -37,70 +37,77 @@ function Schedule() {
             </tr>
 
             <tr>
-              <td data-label="Time">12:00 PM</td>
+              <td data-label="Time">12:00 PM - 12:55 PM</td>
               <td data-label="Event">Technical Interview Prep</td>
               <td data-label="Organizer">Association for Computing Machinery (ACM)</td>
               <td data-label="Location">ENB 116</td>
             </tr>
             
             <tr>
-              <td data-label="Time">1:00 PM</td>
+              <td data-label="Time">12:00 PM - 12:55 PM</td>
               <td data-label="Event">How to Present Your Project</td>
+              <td data-label="Organizer">Society of Professional Hispanic Engineers (SHPE)</td>
+              <td data-label="Location">ENB 118</td>
+            </tr>
+            
+            <tr>
+              <td data-label="Time">1:00 PM - 1:55 PM</td>
+              <td data-label="Event">Web3 and Crypto Basics Workshop</td>
               <td data-label="Organizer">Society of Professional Hispanic Engineers (SHPE)</td>
               <td data-label="Location">ENB 118</td>
             </tr>
 
             <tr>
-              <td data-label="Time">1:00 PM</td>
+              <td data-label="Time">1:00 PM - 1:55 PM</td>
               <td data-label="Event">Competitive Coding Challenge</td>
               <td data-label="Organizer">Association for Computing Machinery (ACM)</td>
               <td data-label="Location">ENB 116</td>
             </tr>
 
             <tr>
-              <td data-label="Time">2:00 PM</td>
+              <td data-label="Time">1:55 PM - 2:25 PM</td>
               <td data-label="Event">Lunch</td>
               <td data-label="Organizer">Everyone</td>
               <td data-label="Location">ENB</td>
             </tr>
 
             <tr>
-              <td data-label="Time">2:15 PM - 4:00pm</td>
-              <td data-label="Event">Website Development</td>
+              <td data-label="Time">2:15 PM - 3:55 PM</td>
+              <td data-label="Event">Website DeveloPMent</td>
               <td data-label="Organizer">IEEE Computer Society</td>
               <td data-label="Location">ENB 116</td>
             </tr>
 
             <tr>
-              <td data-label="Time">2:15 PM - 4:00pm</td>
+              <td data-label="Time">2:15 PM - 3:55 PM</td>
               <td data-label="Event">Internet of Things with Amazon Echo</td>
               <td data-label="Organizer">IEEE Computer Society</td>
               <td data-label="Location">ENB 118</td>
             </tr>
 
             <tr>
-              <td data-label="Time">4:00 PM</td>
+              <td data-label="Time">4:00 PM - 4:55 PM</td>
               <td data-label="Event">Git Fundamentals</td>
               <td data-label="Organizer">Women in Computer Science and Engineering (WiCSE)</td>
               <td data-label="Location">ENB 116</td>
             </tr>
 
             <tr>
-              <td data-label="Time">4:00 PM</td>
+              <td data-label="Time">4:00 PM - 4:55 PM</td>
               <td data-label="Event">How to Succeed in Hackathons</td>
-              <td data-label="Organizer">Google Development Student Club (GDSC)</td>
+              <td data-label="Organizer">Google DeveloPMent Student Club (GDSC)</td>
               <td data-label="Location">ENB 118</td>
             </tr>
 
             <tr>
-              <td data-label="Time">5:00 PM</td>
+              <td data-label="Time">5:00 PM - 5:55 PM</td>
               <td data-label="Event">SQL Basics for Data Science</td>
               <td data-label="Organizer">Girls Who Code (GWC)</td>
               <td data-label="Location">ENB 116</td>
             </tr>
 
             <tr>
-              <td data-label="Time">5:00 PM</td>
+              <td data-label="Time">5:00 PM - 5:55 PM</td>
               <td data-label="Event">Deep Learning with PyTorch</td>
               <td data-label="Organizer">Data Science Club (DSC)</td>
               <td data-label="Location">ENB 118</td>
@@ -108,14 +115,14 @@ function Schedule() {
 
 
             <tr>
-              <td data-label="Time">6:00 PM</td>
+              <td data-label="Time">6:00 PM - 7:00 PM</td>
               <td data-label="Event">Dinner</td>
               <td data-label="Organizer">Everyone</td>
               <td data-label="Location">ENB</td>
             </tr>
 
             <tr>
-              <td data-label="Time">8:00 PM</td>
+              <td data-label="Time">7:30 PM - 8:30 PM</td>
               <td data-label="Event">Team Games</td>
               <td data-label="Organizer">Everyone</td>
               <td data-label="Location">HALL OF FLAGS (ENB II)</td>
@@ -129,7 +136,7 @@ function Schedule() {
             </tr>
 
             <tr>
-              <td data-label="Time">10:00 PM</td>
+              <td data-label="Time">10:00 PM - 10:30 PM</td>
               <td data-label="Event">Closing Ceremony and Prizes</td>
               <td data-label="Organizer">Everyone</td>
               <td data-label="Location">ENB</td>
